d9442d8092c05eca38e300b5109c1fc961215b08
SilentWiskers <cbustam@g.clemson.edu>
2019-12-09 14:00:04 -0500

    print in bitshift

--- bitshift.java
+++ bitshift.java
@@ -29,6 +29,32 @@ public class bitshift{
 
 	}
 
+	public static void printLine(int wor, int addr, int tag, int index, int offset, int status, int memref){
+                String access;
+                String memstat;
+                if (wor == 0){access = "read";}
+                else{access = "write";}
+
+                if (status == 0){memstat = "miss";}
+                else{memstat = "hit";}
+
+                System.out.printf("%6s%8s%6s%7s%8s%7s%8s%n", access, addr, tag, index, offset, memstat, memref);
+
+        }
+
+        public static void printStat(int hit, int miss,  int access){
+                System.out.println("");
+                System.out.println("Simulation Summary Statistics");
+                System.out.println("-----------------------------");
+                System.out.println("Total hits       : " + hit);
+                System.out.println("Total misses     : " + miss);
+                System.out.println("Total accessess  : " + access);
+
+                float hitr = (hit/(float)access);
+                float missr = (miss/(float)access);
+                System.out.println("Hit ratio        : " + hitr);
+                System.out.println("Miss ratio       : " + missr);
+        }
 
 
 }
